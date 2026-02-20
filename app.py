a22f60f137c4eea64cf2f99b6080d105818e3ef7
Pra-702047 <pratham.uttarwar@gmail.com>
2026-02-20 16:31:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd     
+import numpy as np
+#desplaying text content
+st.title("My first streamlit app")
+s.write("This is my first streamlit app. I am learning how to use streamlit to create interactive web applications.")
+st.text("Streamlit is a great tool for data scientists and machine learning engineers to create interactive web applications without needing to know HTML, CSS, or JavaScript.")
